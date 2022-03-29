abd20a30407fe85e81ff091b8c3902213f5e0551
Shorn <bitbucket-shorn@kopi.cloud>
2022-03-29 11:13:45 +1000

    add google idp pool

--- terraform/src/main/terraform/bucket-cognito-poc-state.tf
+++ terraform/src/main/terraform/bucket-cognito-poc-state.tf
@@ -1,13 +1,3 @@
-#resource "aws_s3_bucket_object" "readme"{
-#  bucket = "cognito-poc-state"
-#  key = "readme.txt"
-#  content_type = "text/plain"
-#  content = <<-EOT
-#    This bucket stores the Terraform state.
-#    Bucket was created via the createS3StateBucket task.
-#  EOT
-#}
-
 resource "aws_s3_object" "readme_state_object"{
   bucket = "cognito-poc-state"
   key = "readme.txt"
