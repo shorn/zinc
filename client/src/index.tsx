b290ebf4d343c40befa5f3e4f6aa5744b867bb1e
Shorn <bitbucket-shorn@kopi.cloud>
2022-05-19 10:36:54 +1000

    enable react StrictMode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,17 +6,16 @@ import {App} from "App";
 import { authApi } from "Api/AuthApi";
 import { ServerInfo } from "Shared/ApiTypes";
 
-/* load this as early as possible to trigger lambda cold start.
-There's gotta be a better way, I'd prefer this was issued directly from 
-index.html, even before any script is loaded - but `<link rel="preload">` 
-doesn't seem to work. 
-Unless I put the lambda as the target of the `shortcut icon` (favicon) link, 
-but that's icky.
-*/
+/* load this as early as possible to trigger lambda cold start. */
 export const serverConfigRequest: Promise<ServerInfo> = authApi.readConfig();
 (async ()=> await serverConfigRequest)();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
